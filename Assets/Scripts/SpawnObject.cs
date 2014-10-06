3c4c274e663b47d85f6b4a3213805cf6750f64bb
Dave <daveyboywalton@gmail.com>
2014-10-06 17:05:18 +0100

    Setting up player build UI & object spawner.

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -3,42 +3,53 @@ using System.Collections;
 
 public class SpawnObject : MonoBehaviour 
 {
-
+	
 	[System.Serializable] struct rule
 	{
-
+		public string spawnableObjectName;
 		public PrefabPool prefabPool;
 		public int minSpawnDistance;
 		public int maxSpawnDistance;
 		public int maxObjectsOnScreen;
-		public int distanceSinceLastSpawn;
+		public int myNextSpawnPoint;
+		public int myLastSpawnPosition;
 	}
-
+	
 	[SerializeField] rule[] ruleSets;
 	[SerializeField] int cameraDistanceCovered = 0;
 	[SerializeField] int cameraStartingXPosition = 0;
 	private GameObject spawnPoint0;
 	private int currentObjectsOnScreen = 0;
-
+	
 	void Awake()
 	{
 		cameraStartingXPosition = ( int )Camera.main.transform.position.x;
 		spawnPoint0 = GameObject.FindWithTag ("SP0");
 	}
-
-
+	
+	
 	void Update()
 	{
-		cameraDistanceCovered += ( int ) Camera.main.transform.position.x - cameraStartingXPosition;
+		cameraDistanceCovered = ( int ) Camera.main.transform.position.x - cameraStartingXPosition;
 
+		for(int i = 0; i < ruleSets.Length; i++)
+		{ 
 
-		foreach(rule myRule in ruleSets)
-		{
-			int myRandomSpawnDistance = Random.Range( myRule.minSpawnDistance, myRule.maxSpawnDistance + 1 );
-			if( myRule.distanceSinceLastSpawn >= myRandomSpawnDistance )
+			if( ruleSets[i].myNextSpawnPoint <= spawnPoint0.transform.position.x )
 			{
-				myRule.prefabPool.Spawn( spawnPoint0.transform.position, spawnPoint0.transform.rotation );
+				int myRandomSpawnLane = Random.Range(-2, 3);
+				GameObject spawnedObject = ruleSets[i].prefabPool.Spawn( new Vector3(spawnPoint0.transform.position.x, myRandomSpawnLane, spawnPoint0.transform.position.z), spawnPoint0.transform.rotation ); 
+				ruleSets[i].myNextSpawnPoint = (int)spawnPoint0.transform.position.x + Random.Range( ruleSets[i].minSpawnDistance, ruleSets[i].maxSpawnDistance + 1 );
+				ruleSets[i].myLastSpawnPosition = (int)spawnedObject.transform.position.x;
+
+				foreach (Renderer rend in (spawnedObject.GetComponentsInChildren<Renderer>()))
+	         	{
+					rend.sortingLayerName = "Lane_" + myRandomSpawnLane.ToString(); 
+				}
 			}
 		}
 	}
 }
+
+
+	
