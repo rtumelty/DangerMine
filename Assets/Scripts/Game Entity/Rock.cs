4379ad35831dcdaad6a79ca6dd833ee24c878eb0
Ronan Tumelty <rtumelty@gmail.com>
2014-10-08 15:24:42 +0100

    Implementing rocks

---
+++ Assets/Scripts/Game Entity/Rock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : GameEntity {
+	[SerializeField] int goldPerHit = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
