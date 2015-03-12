77d92a0ceaa0b9853f3e98bd20857e105951a8af
Ronan Tumelty <rtumelty@gmail.com>
2015-03-12 02:12:12 +0000

    Implemented A* pathfinding

---
+++ Assets/Scripts/Gameplay/Pathfinding/AStar.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AStar {
+	public const int cost = 10;
+	public const int diagonalCost = 14;
+	const int iterationLimit = 3;
+
+	/// <summary>
+	/// Searches adjacent positions to find shortest path to end position. 
+	/// Iterations limited to prevent infinite searches if no valid path.
+	/// </summary>
+	/// <returns>If successful, returns the shortest path as estimated by the heuristic algorithm. If no path is found, returns null.</returns>
+	/// <param name="position">Origin position of the search.</param>
+	/// <param name="endPosition">The target position of the search.</param>
+	public static List<GridCoordinate> GetPath(GridCoordinate startPosition, GridCoordinate endPosition) {
+		List<Node> openList = new List<Node>();
+		List<Node> closedList = new List<Node>();
+
+		// Move start position to open list
+		openList.Add(new Node(null, startPosition, endPosition));
+
+		// Execute pathfinding algorithm on adjacent nodes
+		while (openList.Count > 0) {
+			Node lowestF = null;
+			foreach (Node node in openList) {
+				if (lowestF == null) {
+					lowestF = node;
+				}
+				else if (node.f < lowestF.f)
+					lowestF = node;
+			}
+
+			openList.Remove(lowestF);
+			closedList.Add(lowestF);
+
+			if (lowestF.position == endPosition) {
+				List<GridCoordinate> path = new List<GridCoordinate>();
+				Node nextNode = lowestF;
+
+				while (nextNode != null) {
+					path.Insert(0, nextNode.position);
+					nextNode = nextNode.parent;
+				}
+
+				return path;
+			} 
+			else
+				FindAdjacents(lowestF, openList, closedList, endPosition);
+		}
+
+		return null;
+	}
+
+	static void FindAdjacents(Node node, List<Node> openList, List<Node> closedList, GridCoordinate endPosition) {
+		// Add adjacent nodes to list
+		for (int i = -1; i < 2; i++) {
+			if (node.position.y + i >= GridManager.minY && node.position.y + i <= GridManager.maxY) {
+				for (int j = -1; j < 2; j++) {
+					if (node.position.x + j >= GridManager.minScreenX && node.position.x + i <= GridManager.maxScreenX && !(i == 0 && j == 0)) {
+						GridCoordinate nextPosition = node.position + new GridCoordinate(j, i);
+						
+						Node newNode = new Node(node, nextPosition, endPosition);
+
+						if (closedList.Contains(newNode) || OccupiedNode(nextPosition)) {}
+						else if (openList.Contains(newNode)) {
+							int index = openList.IndexOf(newNode);
+							Node oldNode = openList[index];
+
+							if (oldNode.g > newNode.g) {
+								openList[index] = newNode;
+							}
+						}
+						else {
+							openList.Add(newNode);
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	static bool OccupiedNode(GridCoordinate coord) {
+		if (!GridManager.Instance.IsOccupied(GridManager.Grid.WorldGrid, coord))
+			return false;
+		else {
+			List<GameEntity> entities = GridManager.Instance.EntitiesAt(GridManager.Grid.WorldGrid, coord);
+
+			foreach (GameEntity entity in entities) {
+				if (entity is Enemy || entity is Obstacle)
+					return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/Gameplay/Pathfinding/Node.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Node : IEquatable<Node> {
+	
+	public GridCoordinate position;
+	public Node parent;
+	public int f;
+	public int g;
+	public int h;
+
+	public Node(Node newParent, GridCoordinate newPosition, GridCoordinate endPosition) {
+		parent = newParent;
+		position = newPosition;
+
+		if (parent == null) g = 0;
+		else g = newParent.g;
+
+		if ((newPosition.x - parent.position.x) != 0 && (newPosition.y - parent.position.y) != 0)
+			g += AStar.diagonalCost;
+		else
+			g += AStar.cost;
+		
+		h = CalculateHeuristic(newPosition, endPosition);
+		f = g + h;
+	}
+	
+	int CalculateHeuristic(GridCoordinate startPosition, GridCoordinate endPosition) {
+		// Manhattan
+		return (Mathf.Abs(endPosition.x - startPosition.x) + Mathf.Abs(endPosition.y - startPosition.y)) * AStar.cost;
+	}
+	
+	public static bool operator ==(Node a, Node b) {
+		if (a == null || b == null)
+			return false;
+
+		if (a.position == b.position)
+			return true;
+		
+		return false;
+	}
+	
+	public static bool operator !=(Node a, Node b) {
+		return !(a == b);
+	}
+	
+	public bool Equals(Node otherNode) {
+		if (otherNode == null) return false;
+		
+		if (otherNode.position == this.position)
+			return true;
+		else return false;
+	}
+	
+	public override bool Equals(System.Object other) {
+		if (other == null) return false;
+		
+		Node otherNode = other as Node;
+		if (otherNode == null) return false;
+
+		return Equals(otherNode);
+	}
+
+	public override int GetHashCode ()
+	{
+		return this.position.GetHashCode ();
+	}
+}
